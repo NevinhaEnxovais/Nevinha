ca4c97972686dc3d958f4b700948d554c87e0275
Nevinha Enxovais <nevinhamesabanho091@gmail.com>
2026-03-04 11:54:47 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -37,7 +37,7 @@ const listaProdutos = [
     { nome: "Jogo lençol Premium Queen", categoria: "queen", preco: 165.00, imagem: "https://i.postimg.cc/7YXb3hFZ/premiun-Queen.jpg", descricao: "4 Peças - 100% Algodão" },
     { nome: "Jogo lençol Ambar Queen", categoria: "queen", preco: 329.00, imagem: "https://i.postimg.cc/X7xXfqRS/ambar-queen.png", descricao: "4 Peças - Döhler" },
     { nome: "Jogo lençol Teka Queen", categoria: "queen", preco: 249.00, imagem: "https://i.postimg.cc/gk4rVjCx/teka-queen.png", descricao: "150 Fios - 4 Peças" },
-    { nome: "Kit colcha Piquet Queen", categoria: "queen", preco: 329.00, imagem: "https://i.postimg.cc/X7xXfqRN/piquetqueen.png", descricao: "100% Algodão Dohler" },
+    { nome: "Kit colcha Piquet Queen", categoria: "queen", preco: 359.00, imagem: "https://i.postimg.cc/X7xXfqRN/piquetqueen.png", descricao: "100% Algodão Dohler" },
     { nome: "Kit colcha Plaza Queen", categoria: "queen", preco: 449.00, imagem: "https://i.postimg.cc/bNLdxr7N/plaza-queen.png", descricao: "Lavive Luxo 3 peças" },
     { nome: "Kit colcha Londres Queen", categoria: "queen", preco: 299.00, imagem: "https://i.postimg.cc/Dy5mr09h/londresqueen.png", descricao: "Matelassê 3 peças" },
     { nome: "Kit colcha Sicilia Queen", categoria: "queen", preco: 199.00, imagem: "https://i.postimg.cc/zXp3Cvs3/sicili.png", descricao: "Ultrassônica 3 peças" },
